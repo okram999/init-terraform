454de05a5fed460a8564ee5ea5e269a3f64ef234
Niris Okram <niris@amazon.com>
2020-12-20 12:46:22 -0600

    initial commit with s3 backend

---
+++ example.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket = "niris-terraform-backend-store"
+    encrypt = true
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock-dynamo" 
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-04d29b6f966df1537"
+    "us-west-2" = "ami-08d70e59c07c61a3a"
+  }
+}
